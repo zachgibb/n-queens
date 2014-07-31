fc9f48676f63ae1f5f1182765af1a446e2c489e7
Zach Gibb <zachgibb@gmail.com>
2014-07-31 00:36:53 -0400

    fixed countNQueens. need to clean up, but it works

--- src/solvers.js
+++ src/solvers.js
@@ -45,11 +45,26 @@ window.countNRooksSolutions = function(n) {
   }
 
   solutionCount = factorial(n);
-  
+
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
 };
 
+
+
+var enoughQueens = function(board) {
+  var numRows = board.get('n');
+  var numQueens = 0;
+  for(var i = 0; i < numRows; i++) {
+    var currentRows = board.get(i);
+    for(var j = 0; j < numRows; j++) {
+      if(currentRows[j] === 1){
+        numQueens++;
+      }
+    }
+  }
+  return numQueens === numRows;
+};
 
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
@@ -63,8 +78,48 @@ window.findNQueensSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
-  var solutionCount = undefined; //fixme
-
-  console.log('Number of solutions for ' + n + ' queens:', solutionCount);
-  return solutionCount;
+  var masterBoard = new Board({n: n});
+  var numRows = n;
+  // iterate through rows
+  var count = 0;
+  // pull the matrix here so we dont have to keep making new boards
+  var recursion = function (board, row, col) {
+    for (var i = row; i < numRows; i++) {
+      var currentRow = board.get(i);
+      // iterate through indices
+      var j = 0;
+      if(i === row) {
+        j = col;
+      }
+      for (; j < numRows; j++) {
+        // clone current board
+        var cloneMatrix = [];
+        for (var k = 0; k < numRows; k++) {
+          var cloneRow = board.get(k).slice();
+          cloneMatrix.push(cloneRow);
+        }
+        
+        cloneMatrix = new Board(cloneMatrix);
+        
+        // set our current position to 1 on the clone
+        // if current square is 0
+        currentRow[j] = 1;
+        var majorMagicNumber = j - i;
+        var minorMagicNumber = j + i;
+        if (board.hasRowConflictAt(i) || board.hasColConflictAt(j)
+              || board.hasMajorDiagonalConflictAt(majorMagicNumber)
+              || board.hasMinorDiagonalConflictAt(minorMagicNumber)) {
+          currentRow[j] = 0;
+        } else {
+          recursion(cloneMatrix, i, j+1);
+        }
+      }
+    }
+    if(enoughQueens(board)) {
+      count++;
+    }
+  };
+  recursion(masterBoard, 0, 0);
+  console.log('Number of solutions for ' + n + ' queens:', count);
+  return count;
 };
